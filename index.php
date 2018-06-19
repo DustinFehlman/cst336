9998d5e934dcdd7e579a3c656fa8c00ea33b3d29
Dustin Fehlman <dfehlman@csumb.edu>
2018-06-19 09:44:52 +0000

    Added basic homepage nav.

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <html><body>
+    <table>
+        <tr>
+            <th>Labs</th>
+        </tr>
+        <tr>
+            <td>
+                <a href="labs/lab1/index.html">Lab 1</a>
+            </td>
+        </tr>
+    </table>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-
-echo 'Hello world from Dustin F!';
-
 ?>
 </body>
 </html>
